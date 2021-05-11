2ae94c2cb764808c6adaefb382b50f71f51d3ecf
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-05-11 17:07:20 +1200

    Add publishing support to build.gradle.kts

--- tts-ktx/build.gradle.kts
+++ tts-ktx/build.gradle.kts
@@ -1,6 +1,10 @@
+import Publishing.configureMavenPublication
+
 plugins {
     id("com.android.library")
     id("kotlin-android")
+    id("maven-publish")
+    id("signing")
 }
 
 android {
@@ -36,4 +40,44 @@ android {
 dependencies {
     api("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.0-RC")
     testImplementation("junit:junit:4.13.2")
+}
+
+// Bundle sources with binaries
+val androidSourcesJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
+    from(android.sourceSets["main"].kotlin.name)
+}
+artifacts {
+    archives(androidSourcesJar)
+}
+
+publishing {
+    publications {
+        create(
+            "release",
+            configureMavenPublication(
+                "tts-ktx",
+                "Kotlin extensions for Android's TextToSpeech class",
+                "https://github.com/boswelja/tts-ktx",
+                project.configurations.implementation.get().allDependencies
+            ) {
+                artifact("$buildDir/outputs/aar/${project.name}-release.aar")
+                artifact(androidSourcesJar)
+            }
+        )
+    }
+    repositories(Publishing.repositories)
+}
+
+// Create signing config
+ext["signing.keyId"] = Publishing.signingKeyId
+ext["signing.password"] = Publishing.signingPassword
+ext["signing.secretKeyRingFile"] = Publishing.signingSecretKeyring
+signing {
+    sign(publishing.publications)
+}
+
+// Make publish task depend on assembleRelease
+tasks.named("publishReleasePublicationToSonatypeRepository") {
+    dependsOn("assembleRelease")
 }
