c6370736be56ba437db7d6bb683b6d19e961d902
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-05-25 12:16:34 +1200

    Update Gradle and Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 buildscript {
-    val kotlinVersion = "1.4.32"
+    val kotlinVersion = "1.5.0"
 
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-alpha15")
+        classpath("com.android.tools.build:gradle:7.1.0-alpha01")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.0")
     }
 }
