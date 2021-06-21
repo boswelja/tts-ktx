88edfde805772aee3945e0b1fe0e184fc389932e
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-06-21 17:01:41 +1200

    Log status when TTS init fails

--- tts-ktx/src/main/kotlin/com/boswelja/tts/OnInitListener.kt
+++ tts-ktx/src/main/kotlin/com/boswelja/tts/OnInitListener.kt
@@ -6,6 +6,6 @@ internal class OnInitListener : TextToSpeech.OnInitListener {
     override fun onInit(status: Int) {
         // If init wasn't successful, we throw an exception.
         if (status != TextToSpeech.SUCCESS)
-            throw IllegalStateException("Failed to init TextToSpeech")
+            throw IllegalStateException("Failed to init TextToSpeech with status $status")
     }
 }
